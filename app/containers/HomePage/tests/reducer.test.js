20a052eb576403a19c45f108a6f5d3d9c35787dd
vpste1 <vaughan.stedman@gmail.com>
2018-03-01 13:10:40 +1100

    Reducer testing coverage

--- app/containers/HomePage/tests/reducer.test.js
+++ app/containers/HomePage/tests/reducer.test.js
@@ -2,6 +2,7 @@
 import homePageReducer from '../reducer';
 
 import {
+  defaultAction,
   pageLoadAction,
   retrieveURLsSuccess,
   retrieveURLsFailure } from '../actions';
@@ -13,11 +14,14 @@ describe('Container [App] - reducers', () => {
   it('should return the initial state', () => {
     expect(homePageReducer(undefined, {})).toMatchSnapshot();
   });
+  it('should handle the defaultAction action correctly', () => {
+    expect(homePageReducer(state, defaultAction())).toMatchSnapshot();
+  });
   it('should handle the pageLoadAction action correctly', () => {
     expect(homePageReducer(state, pageLoadAction())).toMatchSnapshot();
   });
   it('should handle the retrieveURLsSuccess action correctly', () => {
-    expect(homePageReducer(state, retrieveURLsSuccess(token))).toMatchSnapshot();
+    expect(homePageReducer(state, retrieveURLsSuccess())).toMatchSnapshot();
   });
   it('should handle the retrieveURLsFailure action correctly', () => {
     expect(homePageReducer(state, retrieveURLsFailure())).toMatchSnapshot();
